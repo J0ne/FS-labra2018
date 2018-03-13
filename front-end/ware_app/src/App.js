77ec59ca31a417e989ffc6bc914698426214ff91
Jouni Männistö <jouni.mannisto@gmail.com>
2018-03-13 22:02:20 +0200

    Kirjastoja: mm. axios, alustusta

--- front-end/ware_app/src/App.js
+++ front-end/ware_app/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider} from 'react-redux'
+import productService from './services/products'
 
 class App extends Component {
+
+  componentDidMount() {
+    
+    const all = productService.getAll()
+  }
   render() {
     return (
+      <Provider>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        Hello
         </header>
+        {/* <ul>
+        {products.getAll().map( p => <li key={p.id}>{p.name}</li>)}
+        </ul> */}
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      </Provider>
     );
   }
 }
