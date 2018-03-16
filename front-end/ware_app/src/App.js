871b95c1b26d8e2a20f5451ab085a67dd61dd21e
Jouni Männistö <jouni.mannisto@gmail.com>
2018-03-16 20:04:33 +0200

    Ohjelman runkoa (SPA) routet, navigointi, dummy-dataa

--- front-end/ware_app/src/App.js
+++ front-end/ware_app/src/App.js
@@ -4,29 +4,64 @@ import productService from './services/products'
 import ConnectedProductList from './components/ProductList'
 import { productInitialization } from './reducers/productReducer'
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, NavLink, Link, Redirect } from 'react-router-dom'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+  Input
+} from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      products: []
+      products: [],
+      activeItem: 'Lainaukset'
     }
-  }
+}
 
+  handleItemClick = (e, { name }) => { 
+    console.log(name)
+    this.setState({ activeItem: name })}
+  
   componentDidMount = async () => {
     this.props.productInitialization()
 
   }
   render() {
+
+    const { activeItem } = this.state
     return (
-      <div className="App">
-        <ul>
-        {this.state.products.map( p => <li key={p.id}>{p.nimi}</li>)}
-        </ul>
-        <p className="App-intro">
-        </p>
-        <ConnectedProductList store={this.props.store} />
-      </div>
+      <Router>
+      <Container>
+        <Menu secondary>
+          <Menu.Item name='Lainaukset' as={NavLink} to={"/lainaukset"} active={activeItem === 'Lainaukset'} onClick={this.handleItemClick} />
+            <Menu.Item name='Varasto' as={NavLink} to={"/varasto"} active={activeItem === 'Varasto'} onClick={this.handleItemClick} />
+            <Menu.Item name='Asiakkaat' as={NavLink} to={"/asiakkaat"} active={activeItem === 'Asiakkaat'} onClick={this.handleItemClick} />
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Input icon='search' placeholder='Search...' />
+            </Menu.Item>
+            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+          </Menu.Menu>
+        </Menu>
+          <Route exact path="/lainaukset" render={({ match }) => <div><h1>Lainaukset</h1></div>} />
+          <Route exact path="/varasto" render={() => <ConnectedProductList store={this.props.store} />} />
+
+          <Route exact path="/asiakkaat" render={({ match }) => <div><h1>Asiakkaat</h1></div>} />
+      </Container>
+      </Router>
     );
   }
 }
---
+++ front-end/ware_app/src/services/lendings.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const baseUrl = "http://localhost:3001/lendings"
+
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+export default { getAll } 
